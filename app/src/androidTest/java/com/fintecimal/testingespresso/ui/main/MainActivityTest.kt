e6f981d2482531252ad05554a98b930d88f24e92
Aaron <aaronbautistalopez@gmail.com>
2021-05-29 18:01:05 -0500

    haciendo testing a la navegacion

--- app/src/androidTest/java/com/fintecimal/testingespresso/ui/main/MainActivityTest.kt
+++ app/src/androidTest/java/com/fintecimal/testingespresso/ui/main/MainActivityTest.kt
@@ -1,7 +1,9 @@
 package com.fintecimal.testingespresso.ui.main
 
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
@@ -18,7 +20,6 @@ class MainActivityTest{
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
         onView(withId(R.id.main)).check(matches(isDisplayed()))
     }
-
     // Visibility
     @Test
     fun testVisibility_title_nextButton() {
@@ -33,8 +34,6 @@ class MainActivityTest{
         onView(withId(R.id.button_next_activity))
             .check(matches(isDisplayed()))
     }
-
-
     @Test
     fun testTitleTextDisplayed() {
         val activityScenario = ActivityScenario.launch(MainActivity::class.java)
@@ -42,6 +41,30 @@ class MainActivityTest{
         onView(withId(R.id.activity_main_title))
             .check(matches(withText(R.string.text_mainactivity)))
     }
+
+
+
+    @Test
+    fun test_backPress_toMainActivity() {
+
+        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.button_next_activity)).perform(ViewActions.click())
+
+        onView(withId(R.id.secondary)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_back)).perform(ViewActions.click()) // method 1
+
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.button_next_activity)).perform(ViewActions.click())
+
+        Espresso.pressBack() // method 2
+
+        onView(withId(R.id.main)).check(matches(isDisplayed()))
+    }
+
+
 
 }
 
---
+++ app/src/main/java/com/fintecimal/testingespresso/SecondaryActivity.kt
@@ -0,0 +1,19 @@
+package com.fintecimal.testingespresso
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SecondaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_secondary)
+
+        val buttonBack = findViewById<Button>(R.id.button_back)
+
+        buttonBack.setOnClickListener {
+            onBackPressed()
+        }
+    }
+}
